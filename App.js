70b80789974dd73e4df728ee7c57beefbee6b122
Shivam Kakkar <37249646+shivamkakkar4@users.noreply.github.com>
2021-03-11 20:17:21 +0530

    Created CreateEmployee Screen

--- App.js
+++ App.js
@@ -3,11 +3,13 @@ import React from "react";
 import { StyleSheet, Text, View } from "react-native";
 import Constants from "expo-constants";
 import Home from "./screens/Home";
+import CreateEmployee from "./screens/CreateEmployee";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Home />
+      {/* <Home /> */}
+      <CreateEmployee />
       <StatusBar style="auto" />
     </View>
   );
---
+++ screens/CreateEmployee.js
@@ -0,0 +1,76 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { TextInput } from "react-native-paper";
+
+const CreateEmployee = () => {
+  const [name, setName] = useState("");
+  const [phone, setPhone] = useState("");
+  const [email, setEmail] = useState("");
+  const [salary, setSalary] = useState("");
+  const [picture, setPicture] = useState("");
+  const [position, setPosition] = useState("");
+  const [modal, setModal] = useState(false);
+
+  return (
+    <View style={styles.root}>
+      <TextInput
+        label="Name"
+        style={styles.inputStyle}
+        value={name}
+        theme={theme}
+        mode="outlined"
+        onChangeText={text => setName(text)}
+      />
+      <TextInput
+        label="Email"
+        style={styles.inputStyle}
+        value={email}
+        theme={theme}
+        mode="outlined"
+        onChangeText={text => setEmail(text)}
+      />
+      <TextInput
+        label="Phone"
+        style={styles.inputStyle}
+        value={phone}
+        theme={theme}
+        keyboardType="number-pad"
+        mode="outlined"
+        onChangeText={text => setPhone(text)}
+      />
+      <TextInput
+        label="Salary"
+        style={styles.inputStyle}
+        value={salary}
+        theme={theme}
+        mode="outlined"
+        onChangeText={text => setSalary(text)}
+      />
+      <TextInput
+        label="Position"
+        style={styles.inputStyle}
+        value={position}
+        theme={theme}
+        mode="outlined"
+        onChangeText={text => setPosition(text)}
+      />
+    </View>
+  );
+};
+
+const theme = {
+  colors: {
+    primary: "#006aff",
+  },
+};
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+  },
+  inputStyle: {
+    margin: 5,
+  },
+});
+
+export default CreateEmployee;
